d8825ee736de696245f332fcb1a07b710a703689
daftcat1 <978873615@qq.com>
2020-06-12 01:21:55 +0800

    自动生成代码和商品模块提交

--- haodanmall-product/src/main/java/com/personal/haodanmall/product/HaodanmallProductApplication.java
+++ haodanmall-product/src/main/java/com/personal/haodanmall/product/HaodanmallProductApplication.java
@@ -1,8 +1,10 @@
 package com.personal.haodanmall.product;
 
+import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+@MapperScan("com.personal.haodanmall.product.dao")
 @SpringBootApplication
 public class HaodanmallProductApplication {
 
--- haodanmall-product/src/test/java/com/personal/haodanmall/product/HaodanmallProductApplicationTests.java
+++ haodanmall-product/src/test/java/com/personal/haodanmall/product/HaodanmallProductApplicationTests.java
@@ -1,13 +1,30 @@
 package com.personal.haodanmall.product;
 
+import com.personal.haodanmall.product.entity.BrandEntity;
+import com.personal.haodanmall.product.service.BrandService;
 import org.junit.jupiter.api.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
 
+import java.util.List;
+
+@RunWith(SpringRunner.class)
 @SpringBootTest
 class HaodanmallProductApplicationTests {
 
+    @Autowired
+    BrandService brandService;
+
     @Test
-    void contextLoads() {
+void contextLoads() {
+
+        BrandEntity brandEntity = new BrandEntity();
+        brandEntity.setName("华为");
+
+        brandService.save(brandEntity);
+        System.out.println("保存成功。。。。");
     }
 
 }
